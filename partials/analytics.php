0195f8aed9b861739924d248a38656d724663cc1
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-03-12 08:50:31 +0530

    google analytics added

--- partials/analytics.php
+++ partials/analytics.php
@@ -4,9 +4,9 @@ echo '<!-- Global site tag (gtag.js) - Google Analytics -->
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
-  gtag('.'js'.', new Date());
+  gtag('.'"js"'.', new Date());
 
-  gtag('.'config'.', '.'UA-177387594-1'.');
+  gtag('.'"config"'.', '.'"UA-177387594-1"'.');
 </script>
 '
 ?>
