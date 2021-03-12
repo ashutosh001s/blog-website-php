05757f023a558934a0d92022fbc13844d236f4ec
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-03-12 08:54:22 +0530

    google analytics added

--- partials/analytics.php
+++ partials/analytics.php
@@ -1,12 +1,12 @@
 <?php
-echo '<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-177387594-1"></script>
+echo"<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="."https://www.googletagmanager.com/gtag/js?id=UA-177387594-1"."></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
-  gtag('.'"js"'.', new Date());
+  gtag('js', new Date());
 
-  gtag('.'"config"'.', '.'"UA-177387594-1"'.');
-</script>
-'
+  gtag('config', 'UA-177387594-1');
+</script>";
+
 ?>
