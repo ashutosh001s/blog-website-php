61eb9a6462dc7c93fc17a9e5b3ece43fa93cfdba
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-03-13 20:03:34 +0530

    about page modified

--- about.php
+++ about.php
@@ -8,6 +8,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <link type="text/css" rel="stylesheet" href="./assets/css/style.css">
+  <link type="text/css" rel="stylesheet" href="./assets/css/footer.css">
+  <link type="text/css" rel="stylesheet" href="./assets/css/bootstrap-override.css">
+  <link type="text/css" rel="stylesheet" href="./assets/css/responsive.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <style>
