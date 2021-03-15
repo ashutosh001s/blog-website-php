b6b8098ade1fb2382e570418223d20abe2cc6318
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-03-15 07:51:51 +0530

    Tiny MCE Editor implemented on write-post.php

--- write-post.php
+++ write-post.php
@@ -20,7 +20,27 @@ echo'<!doctype html>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <style>
+    <script src="https://cdn.tiny.cloud/1/f78fim1rlfmk51mmqwkbubj31o4dwybrbi30kliqoin43ovu/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>';
+  echo" <script>
+    tinymce.init({
+      selector: 'textarea',
+ 
+  plugins: [
+    'advlist autolink link image lists charmap print preview hr anchor pagebreak',
+    'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+    'table emoticons template paste help'
+  ],
+  toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | ' +
+    'bullist numlist outdent indent | link image | print preview media fullpage | ' +
+    'forecolor backcolor emoticons | help',
+  menu: {
+    favs: {title: 'My Favorites', items: 'code visualaid | searchreplace | emoticons'}
+  },
+  menubar: 'favs file edit view insert format tools table help',
+  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+   });
+  </script>";
+  echo'  <style>
     form{
       display: flex;
       width: 100%;
@@ -46,7 +66,9 @@ echo'<!doctype html>
 
       <div class="formLeft">
       <div class="mb-3">
-            <textarea class="form-control" placeholder = "Write Here" id="content" name="content" rows="30"></textarea>
+             <textarea id="mytextarea" name="content" rows="20">
+      Hello, World!
+    </textarea>
           </div>
       </div>
 
