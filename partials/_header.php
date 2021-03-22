9dc5fe09a497c23c8d1cb165a300bf534740a24a
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-03-22 06:48:21 +0530

    add link in logo

--- partials/_header.php
+++ partials/_header.php
@@ -4,7 +4,7 @@ echo ' <!-- *********Navigation start******* -->
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light" id = "navbar">
     <div class="container-fluid">
-    <img class = "logo" src="/assets/img/cover/logo2.png" alt="">
+    <a href="/"><img class = "logo" src="/assets/img/cover/logo2.png" alt=""></a>
       
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
