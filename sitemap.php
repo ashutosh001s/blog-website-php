1dad71cc0b78af12a81960c4a91603ff6e714f95
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-03-22 10:37:54 +0530

    sitemap added v2

--- sitemap.php
+++ sitemap.php
@@ -12,7 +12,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL;
 
 echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">' . PHP_EOL;
 
-while($row = mysqli_fetch_array($result))
+while($row = mysqli_fetch_assoc($result))
 {
  echo '<url>' . PHP_EOL;
  echo '<loc>'.$base_url. $row["page_url"] .'/</loc>' . PHP_EOL;
