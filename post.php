cbbcacef6ec29532f66550e3b8802e0e16d4070c
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-04-05 19:02:09 +0530

    tab spaces removed v10

--- post.php
+++ post.php
@@ -59,8 +59,8 @@ function minifier($code) {
         type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <link rel="stylesheet" href="../assets/css/post.css">
     <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="stylesheet" href="../assets/css/post.css">
     <link rel="stylesheet" href="../assets/css/footer.css">
     <link rel="stylesheet" href="../assets/css/bootstrap-override.css">
     <link rel="stylesheet" href="../assets/css/responsive.css">
