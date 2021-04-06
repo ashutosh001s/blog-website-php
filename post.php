5703c29c309ae37ef8d0d5541d3cc99a0af76e6d
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-04-06 06:30:35 +0530

    roboto font in post.php

--- post.php
+++ post.php
@@ -59,6 +59,8 @@ function minifier($code) {
         type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="../assets/css/post.css">
     <link rel="stylesheet" href="../assets/css/footer.css">
@@ -75,6 +77,9 @@ function minifier($code) {
     
     .bg-light{
       position: relative!important;
+    }
+    body{
+      font-family: 'Roboto', sans-serif;
     }
     </style>
 
