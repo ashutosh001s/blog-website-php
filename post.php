da982691c90d87d6d13370d19c52c58d3702e212
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-04-25 18:06:52 +0530

    search page added v4

--- post.php
+++ post.php
@@ -5,121 +5,120 @@ $time = $time[1] + $time[0];
 $start = $time;
 ?>
 <?php include 'partials/_dbconnect.php'; ?>
-<?php 
-ob_start("minifier"); 
-function minifier($code) { 
-    $search = array( 
-          
-        // Remove whitespaces after tags 
-        '/\>[^\S ]+/s', 
-          
-        // Remove whitespaces before tags 
-        '/[^\S ]+\</s', 
-          
-        // Remove multiple whitespace sequences 
-        '/(\s)+/s', 
-          
-        // Removes comments 
-        '/<!--(.|\s)*?-->/'
-    ); 
-    $replace = array('>', '<', '\\1'); 
-    $code = preg_replace($search, $replace, $code); 
-    return $code; 
-} 
+<?php
+ob_start("minifier");
+function minifier($code)
+{
+  $search = array(
+
+    // Remove whitespaces after tags 
+    '/\>[^\S ]+/s',
+
+    // Remove whitespaces before tags 
+    '/[^\S ]+\</s',
+
+    // Remove multiple whitespace sequences 
+    '/(\s)+/s',
+
+    // Removes comments 
+    '/<!--(.|\s)*?-->/'
+  );
+  $replace = array('>', '<', '\\1');
+  $code = preg_replace($search, $replace, $code);
+  return $code;
+}
 ?>
 <!doctype html>
 <html lang="en">
 
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
 
-    <!-- For seo -->
-    <?php
-      $id = $_GET['id'];
-      $sql = "SELECT * FROM `blogs` WHERE url = '$id'";
-      $result = mysqli_query($conn , $sql);
-      $row = mysqli_fetch_assoc($result);
+  <!-- For seo -->
+  <?php
+  $id = $_GET['id'];
+  $sql = "SELECT * FROM `blogs` WHERE url = '$id'";
+  $result = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_assoc($result);
 
-      $title = $row['title'];
-      $description = $row['description'];
-      $keywords = $row['keywords'];
-      $author = $row['author'];
-      
-    
-    echo'
-    <title>'.$title.'</title>
-    <meta name="description" content="'.$description.'">
-    <meta name="keywords" content="'.$keywords.'">
-    <meta name="author" content="'.$author.'">
+  $title = $row['title'];
+  $description = $row['description'];
+  $keywords = $row['keywords'];
+  $author = $row['author'];
+
+
+  echo '
+    <title>' . $title . '</title>
+    <meta name="description" content="' . $description . '">
+    <meta name="keywords" content="' . $keywords . '">
+    <meta name="author" content="' . $author . '">
     <meta name="viewport" content="width=device-width, initial-scale=1">';
-    ?>
-    <!-- Bootstrap CSS -->
-    <link rel="shortcut icon" href="../assets/img/cover/favicon.png" style="filter: drop-shadow(2px 4px 6px black);"
-        type="image/x-icon">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="stylesheet" href="../assets/css/post.css">
-    <link rel="stylesheet" href="../assets/css/footer.css">
-    <link rel="stylesheet" href="../assets/css/bootstrap-override.css">
-    <link rel="stylesheet" href="../assets/css/responsive.css">
-    <?php include 'partials/analytics.php'; ?>
+  ?>
+  <!-- Bootstrap CSS -->
+  <link rel="shortcut icon" href="../assets/img/cover/favicon.png" style="filter: drop-shadow(2px 4px 6px black);" type="image/x-icon">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../assets/css/style.css">
+  <link rel="stylesheet" href="../assets/css/post.css">
+  <link rel="stylesheet" href="../assets/css/footer.css">
+  <link rel="stylesheet" href="../assets/css/bootstrap-override.css">
+  <link rel="stylesheet" href="../assets/css/responsive.css">
+  <?php include 'partials/analytics.php'; ?>
 
-    <style>
+  <style>
     .commentBy {
-        font-size: 18px;
-        font-family: fantasy;
-        color: #008EFF;
+      font-size: 18px;
+      font-family: fantasy;
+      color: #008EFF;
     }
-    
-    .bg-light{
-      position: relative!important;
+
+    .bg-light {
+      position: relative !important;
     }
-    </style>
+  </style>
 
 
 
 </head>
 
 <body>
-    <?php include 'partials/_header.php'; ?>
+  <?php include 'partials/_header.php'; ?>
 
-    <div class="header">
-        <div class="progress-container">
-            <div class="progress-bar" id="myBar"></div>
-        </div>
+  <div class="header">
+    <div class="progress-container">
+      <div class="progress-bar" id="myBar"></div>
     </div>
+  </div>
 
 
-    <?php 
-     
-     $id = $_GET['id'];
-    $sql = "SELECT * FROM `blogs` WHERE url = '$id'";
-    $result = mysqli_query($conn , $sql);
-    $row = mysqli_fetch_assoc($result);
-    $title = $row['title'];
-    $content = $row['content'];
-    $author = $row['author'];
-    $date = $row['date'];
-    $readMin= $row['read_time'];
-    $image= $row['image'];
-    $date = $row['date'];
+  <?php
 
-    echo '   
+  $id = $_GET['id'];
+  $sql = "SELECT * FROM `blogs` WHERE url = '$id'";
+  $result = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_assoc($result);
+  $title = $row['title'];
+  $content = $row['content'];
+  $author = $row['author'];
+  $date = $row['date'];
+  $readMin = $row['read_time'];
+  $image = $row['image'];
+  $date = $row['date'];
+
+  echo '   
     <div class="post" id="post">
-      <h1 class="font1">'.strtoupper($title).'</h1>
-      <img class = "post-image" src="/assets/img/cover/'.$image.'" alt="">
+      <h1 class="font1">' . strtoupper($title) . '</h1>
+      <img class = "post-image" src="/assets/img/cover/' . $image . '" alt="">
       <div class="blogpost-meta">
           <div class="author-info">
               <div>
               <b>
-                Author - '.$author.'
+                Author - ' . $author . '
               </b>
               </div>
-              <div>'.$date.' . '.$readMin.' min read</div>
+              <div>' . $date . ' . ' . $readMin . ' min read</div>
           </div>
           <div class="social">
               <svg width="29" height="29" class="hk"><path d="M22.05 7.54a4.47 4.47 0 0 0-3.3-1.46 4.53 4.53 0 0 0-4.53 4.53c0 .35.04.7.08 1.05A12.9 12.9 0 0 1 5 6.89a5.1 5.1 0 0 0-.65 2.26c.03 1.6.83 2.99 2.02 3.79a4.3 4.3 0 0 1-2.02-.57v.08a4.55 4.55 0 0 0 3.63 4.44c-.4.08-.8.13-1.21.16l-.81-.08a4.54 4.54 0 0 0 4.2 3.15 9.56 9.56 0 0 1-5.66 1.94l-1.05-.08c2 1.27 4.38 2.02 6.94 2.02 8.3 0 12.86-6.9 12.84-12.85.02-.24 0-.43 0-.65a8.68 8.68 0 0 0 2.26-2.34c-.82.38-1.7.62-2.6.72a4.37 4.37 0 0 0 1.95-2.51c-.84.53-1.81.9-2.83 1.13z" style="
@@ -130,55 +129,52 @@ function minifier($code) {
 
               <svg width="29" height="29" class="hk"><path d="M23.2 5H5.8a.8.8 0 0 0-.8.8V23.2c0 .44.35.8.8.8h9.3v-7.13h-2.38V13.9h2.38v-2.38c0-2.45 1.55-3.66 3.74-3.66 1.05 0 1.95.08 2.2.11v2.57h-1.5c-1.2 0-1.48.57-1.48 1.4v1.96h2.97l-.6 2.97h-2.37l.05 7.12h5.1a.8.8 0 0 0 .79-.8V5.8a.8.8 0 0 0-.8-.79" style=" fill: #008EFF;"></path></svg>';
 
-              echo "<img width='29' height='29' src='../assets/img/cover/pdf.svg'  onclick="."printDiv('post','Title')"." alt='pdf'>";
+  echo "<img width='29' height='29' src='../assets/img/cover/pdf.svg'  onclick=" . "printDiv('post','Title')" . " alt='pdf'>";
 
 
-         echo' </div>
+  echo ' </div>
       </div>
-      '.$content.'
+      ' . $content . '
      
     </div>';
 
-    ?>
+  ?>
 
-    <?php
-include 'partials/_dbconnect.php';
-$url= $_SERVER['REQUEST_URI'];
-session_start();
+  <?php
+  include 'partials/_dbconnect.php';
+  $url = $_SERVER['REQUEST_URI'];
+  session_start();
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-  $id = $_GET['id'];
-  $comment = $_POST['comment'];
-  $comment = str_replace("<", "&lt;" , $comment);
-  $comment = str_replace(">", "&gt;" , $comment);
-  date_default_timezone_set("Asia/Kolkata");
-  $today = date("j F, Y");
-  $username = $_SESSION['username'];
-  $useremail = $_SESSION['useremail'];
-  $sql = "INSERT INTO `comments` (`page_no`, `comment`, `comment_date`) VALUES ('$id', '$comment', '$today')";
-  $sql = "INSERT INTO `comments` (`page_no`, `comment_by`, `comment_user_email`, `comment`, `comment_date`) VALUES ('$id', '$username', '$useremail', '$comment', '$today')";
-  $result = mysqli_query($conn, $sql);
-  
-  
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $id = $_GET['id'];
+    $comment = $_POST['comment'];
+    $comment = str_replace("<", "&lt;", $comment);
+    $comment = str_replace(">", "&gt;", $comment);
+    date_default_timezone_set("Asia/Kolkata");
+    $today = date("j F, Y");
+    $username = $_SESSION['username'];
+    $useremail = $_SESSION['useremail'];
+    $sql = "INSERT INTO `comments` (`page_no`, `comment`, `comment_date`) VALUES ('$id', '$comment', '$today')";
+    $sql = "INSERT INTO `comments` (`page_no`, `comment_by`, `comment_user_email`, `comment`, `comment_date`) VALUES ('$id', '$username', '$useremail', '$comment', '$today')";
+    $result = mysqli_query($conn, $sql);
   }
-  
- 
-
-    echo'<div class="commentSection">';
-      
 
 
-       if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
-      echo'<form action="'.$url.'" method = "POST">
+
+  echo '<div class="commentSection">';
+
+
+
+  if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    echo '<form action="' . $url . '" method = "POST">
         <div class="form-floating">
           <label for="commentSection">Comment</label>
           <textarea class="form-control" placeholder="Leave a comment here" name= "comment" id="commentSection" style="height: 100px" required></textarea>
         </div>
         <button type="submit" class="btn btn-light">Submit</button>
       </form>';
-  }
-  else{
-      echo '<div class="alert alert-success commentLog" role="alert">
+  } else {
+    echo '<div class="alert alert-success commentLog" role="alert">
   <h4 class="alert-heading">Comments</h4>
   <p>Please login to comment</p>
   <hr>
@@ -187,49 +183,50 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
   }
 
 
-      $sql = "SELECT * FROM `comments` WHERE page_no = '$id' ORDER BY `comment_no` DESC";
-      $result = mysqli_query($conn , $sql);
-      while($row = mysqli_fetch_assoc($result)){
-        $comment = $row['comment'];
-        $commentBy = $row['comment_by'];
-        $commentDate = $row['comment_date'];
-        
+  $sql = "SELECT * FROM `comments` WHERE page_no = '$id' ORDER BY `comment_no` DESC";
+  $result = mysqli_query($conn, $sql);
+  while ($row = mysqli_fetch_assoc($result)) {
+    $comment = $row['comment'];
+    $commentBy = $row['comment_by'];
+    $commentDate = $row['comment_date'];
 
-      echo'<div class="media comments">
+
+    echo '<div class="media comments">
         <img  src="/assets/img/cover/user.png" class="mr-3 userImage" alt="...">
         <div class="media-body">
-          <h5 class="mt-0 commentBy">Asked by ' .$commentBy. '  On ' .$commentDate. '</h5>
-          <p>'.$comment.'</p>
+          <h5 class="mt-0 commentBy">Asked by ' . $commentBy . '  On ' . $commentDate . '</h5>
+          <p>' . $comment . '</p>
         </div>
       
       </div>';
-      }
-    ?>
-    </div>
+  }
+  ?>
+  </div>
 
 
-    <?php include 'partials/_footer.php'; ?>
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
-    </script>
-    <script src="../assets/js/script.js"></script>
+  <?php include 'partials/_footer.php'; ?>
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
+  </script>
+  <script src="../assets/js/script.js"></script>
 
-    <script>
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
+  <script>
+    // When the user scrolls the page, execute myFunction 
+    window.onscroll = function() {
+      myFunction()
+    };
 
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
-</script>
+    function myFunction() {
+      var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+      var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      var scrolled = (winScroll / height) * 100;
+      document.getElementById("myBar").style.width = scrolled + "%";
+    }
+  </script>
 </body>
 
 </html>
-<?php 
-ob_end_flush(); 
+<?php
+ob_end_flush();
 ?>
 <?php include 'partials/tracker.php'; ?>
--- search.php
+++ search.php
@@ -2,215 +2,59 @@
 <html lang="en">
 
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link rel="shortcut icon" href="../assets/img/cover/favicon.png" style="filter: drop-shadow(2px 4px 6px black);"
-        type="image/x-icon">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="stylesheet" href="../assets/css/post.css">
-    <link rel="stylesheet" href="../assets/css/search.css">
-    <link rel="stylesheet" href="../assets/css/footer.css">
-    <link rel="stylesheet" href="../assets/css/bootstrap-override.css">
-    <link rel="stylesheet" href="../assets/css/responsive.css">
-    <style>
+  <!-- Bootstrap CSS -->
+  <link rel="shortcut icon" href="../assets/img/cover/favicon.png" style="filter: drop-shadow(2px 4px 6px black);" type="image/x-icon">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../assets/css/style.css">
+  <link rel="stylesheet" href="../assets/css/post.css">
+  <link rel="stylesheet" href="../assets/css/search.css">
+  <link rel="stylesheet" href="../assets/css/footer.css">
+  <link rel="stylesheet" href="../assets/css/bootstrap-override.css">
+  <link rel="stylesheet" href="../assets/css/responsive.css">
+  <style>
     body {
-        background-color: whitesmoke;
+      background-color: whitesmoke;
     }
-    </style>
+  </style>
 
-    <title>Hello, world!</title>
+  <title>Hello, world!</title>
 </head>
 
 <body>
-    <?php include 'partials/_header.php'; ?>
+  <?php include 'partials/_header.php'; ?>
 
-    <div class="searchBox" id="searchBox">
+  <div class="searchBox" id="searchBox">
 
-        <div class="searchCard">
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="https://source.unsplash.com/1600x900/?nature,water" alt="...">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
-                </div>
+    <div class="searchContainer">
+      <div class="card searchCard  mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img class="searchImg" src="https://source.unsplash.com/1600x900/?nature,water" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
             </div>
+          </div>
         </div>
-        <div class="searchCard">
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="https://source.unsplash.com/1600x900/?nature,water" alt="...">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="searchCard">
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="https://source.unsplash.com/1600x900/?nature,water" alt="...">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="searchCard">
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="https://source.unsplash.com/1600x900/?nature,water" alt="...">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="searchCard">
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="https://source.unsplash.com/1600x900/?nature,water" alt="...">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="searchCard">
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="https://source.unsplash.com/1600x900/?nature,water" alt="...">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="searchCard">
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="https://source.unsplash.com/1600x900/?nature,water" alt="...">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="searchCard">
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="https://source.unsplash.com/1600x900/?nature,water" alt="...">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="searchCard">
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="https://source.unsplash.com/1600x900/?nature,water" alt="...">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="searchCard">
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="https://source.unsplash.com/1600x900/?nature,water" alt="...">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
+      </div>
     </div>
 
-    <?php include 'partials/_footer.php'; ?>
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
-    </script>
+  </div>
+
+  <?php include 'partials/_footer.php'; ?>
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
+  </script>
 </body>
 
 </html>
