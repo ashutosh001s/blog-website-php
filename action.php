6b0a4cdd5fd9e29d35a89fc7bb405cd75d6c6761
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-04-26 18:08:02 +0530

    action.php url bug fix

--- action.php
+++ action.php
@@ -1,18 +1,19 @@
 <?php
 session_start();
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $fname = $_POST['fname'];
     $email = $_POST['email'];
-    if(!empty($email) && !filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+    $currentUrl = $_POST['currentUrl'];
+    if (!empty($email) && !filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
         // MailChimp API credentials
         $apiKey = '9e825f9aab231924bf44e62d3db62006-us17';
         $listID = '19c24957f2';
-        
+
         // MailChimp API URL
         $memberID = md5(strtolower($email));
-        $dataCenter = substr($apiKey,strpos($apiKey,'-')+1);
+        $dataCenter = substr($apiKey, strpos($apiKey, '-') + 1);
         $url = 'https://' . $dataCenter . '.api.mailchimp.com/3.0/lists/' . $listID . '/members/' . $memberID;
-        
+
         // member information
         $json = json_encode([
             'email_address' => $email,
@@ -21,7 +22,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
                 'FNAME'     => $fname
             ]
         ]);
-        
+
         // send a HTTP POST request with curl
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_USERPWD, 'user:' . $apiKey);
@@ -34,7 +35,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $result = curl_exec($ch);
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
-        
+
         // store the status message based on response code
         if ($httpCode == 200) {
             $_SESSION['msg'] = '<div class="alert alert-success alert-dismissible fade show" role="alert">
@@ -47,7 +48,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
                     $msg = '<div class="alert alert-warning alert-dismissible fade show" role="alert">
             <strong>Congratulations!</strong> You have been already subscribed.
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>';    
+            </div>';
                     break;
                 default:
                     $msg = '<div class="alert alert-danger alert-dismissible fade show" role="alert">
@@ -56,9 +57,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
                   </div>';
                     break;
             }
-            $_SESSION['msg'] = '<p>'.$msg.'</p>';
+            $_SESSION['msg'] = '<p>' . $msg . '</p>';
         }
-    }else{
+    } else {
         $_SESSION['msg'] = '<p><div class="alert alert-warning alert-dismissible fade show" role="alert">
         <strong>OOps</strong> Please Enter a valid email address.
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
@@ -66,4 +67,4 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     }
 }
 // redirect to homepage
-header('location:index.php');
+header("location: $currentUrl");
--- post.php
+++ post.php
@@ -192,6 +192,8 @@ function minifier($code)
                         <div class="mb-3">
                             <input type="email" class="form-control" id="email" name="email"
                                 placeholder="Enter your email">
+                            <input type="hidden" class="form-control" id="email" name="currentUrl"
+                                value="<?php $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?>">
                         </div>
                         <div class="d-grid gap-2">
                             <button class="btn btn-primary btn-mail" type="Submit">Sign Up</button>
