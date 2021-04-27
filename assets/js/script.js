f9fbe336356dfb7180e0f227eb8cacfecd874a2a
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-04-27 11:13:36 +0530

    newsletter popup added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,23 @@
-var doc = new jsPDF();
+// var doc = new jsPDF();
 
-function printDiv(divId,
-  title) {
+// function printDiv(divId,
+//   title) {
 
-  let mywindow = window.open('', 'PRINT', 'height=650,width=900,top=100,left=150');
+//   let mywindow = window.open('', 'PRINT', 'height=650,width=900,top=100,left=150');
 
-  mywindow.document.write(`<html><head><title>${title}</title>`);
-  mywindow.document.write('</head><body >');
-  mywindow.document.write(document.getElementById(divId).innerHTML);
-  mywindow.document.write('</body></html>');
+//   mywindow.document.write(`<html><head><title>${title}</title>`);
+//   mywindow.document.write('</head><body >');
+//   mywindow.document.write(document.getElementById(divId).innerHTML);
+//   mywindow.document.write('</body></html>');
 
-  mywindow.document.close(); // necessary for IE >= 10
-  mywindow.focus(); // necessary for IE >= 10*/
+//   mywindow.document.close(); // necessary for IE >= 10
+//   mywindow.focus(); // necessary for IE >= 10*/
 
-  mywindow.print();
-  mywindow.close();
+//   mywindow.print();
+//   mywindow.close();
 
-  return true;
-}
+//   return true;
+// }
 window.onload = function () {
   setTimeout(function () {
     console.log('work')
