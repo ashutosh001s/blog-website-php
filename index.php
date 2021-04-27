e374a2d81e07176d93e188c34609df2a21e2cb6a
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-04-27 17:16:23 +0530

    bootstrap offline added

--- index.php
+++ index.php
@@ -44,8 +44,9 @@ function minifier($code)
     <!-- Bootstrap CSS -->
     <link rel="shortcut icon" href="./assets/img/cover/favicon.png" style="filter: drop-shadow(2px 4px 6px black);"
         type="image/x-icon">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"> -->
+    <link type="text/css" rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
     <link type="text/css" rel="stylesheet" href="./assets/css/style.css">
     <link type="text/css" rel="stylesheet" href="./assets/css/footer.css">
     <link type="text/css" rel="stylesheet" href="./assets/css/bootstrap-override.css">
@@ -142,10 +143,11 @@ function minifier($code)
 
     </Section>
     <?php include 'partials/_footer.php'; ?>
+    <script src="./bootstrap/js/bootstrap.min.js"></script>
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
-    </script>
+        </script> -->
 
 </body>
 
