c3c92b19bb1bf4cf2a606ba0c0c691ad942420cb
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-04-27 11:16:01 +0530

    newsletter popup added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,27 @@
-// var doc = new jsPDF();
-
-// function printDiv(divId,
-//   title) {
-
-//   let mywindow = window.open('', 'PRINT', 'height=650,width=900,top=100,left=150');
-
-//   mywindow.document.write(`<html><head><title>${title}</title>`);
-//   mywindow.document.write('</head><body >');
-//   mywindow.document.write(document.getElementById(divId).innerHTML);
-//   mywindow.document.write('</body></html>');
-
-//   mywindow.document.close(); // necessary for IE >= 10
-//   mywindow.focus(); // necessary for IE >= 10*/
-
-//   mywindow.print();
-//   mywindow.close();
-
-//   return true;
-// }
+var newsModal = new bootstrap.Modal(document.getElementById('newsletterModal'));
 window.onload = function () {
   setTimeout(function () {
-    console.log('work')
-  }, 2000);
+    newsModal.show()
+  }, 5000);
+}
+
+var doc = new jsPDF();
+
+function printDiv(divId,
+  title) {
+
+  let mywindow = window.open('', 'PRINT', 'height=650,width=900,top=100,left=150');
+
+  mywindow.document.write(`<html><head><title>${title}</title>`);
+  mywindow.document.write('</head><body >');
+  mywindow.document.write(document.getElementById(divId).innerHTML);
+  mywindow.document.write('</body></html>');
+
+  mywindow.document.close(); // necessary for IE >= 10
+  mywindow.focus(); // necessary for IE >= 10*/
+
+  mywindow.print();
+  mywindow.close();
+
+  return true;
 }
