0e2adf0e9af849b564e885137c8bc76476e533ab
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-04-27 18:42:52 +0530

    404 page design customised

--- 404.php
+++ 404.php
@@ -12,6 +12,12 @@ $start = $time;
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
     <link rel="shortcut icon" href="./assets/img/cover/favicon.png" style="filter: drop-shadow(2px 4px 6px black);"
         type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link type="text/css" rel="stylesheet" href="./assets/css/style.css">
+    <link type="text/css" rel="stylesheet" href="./assets/css/footer.css">
+    <link type="text/css" rel="stylesheet" href="./assets/css/bootstrap-override.css">
+    <link type="text/css" rel="stylesheet" href="./assets/css/responsive.css">
 
     <style>
     * {
@@ -194,6 +200,10 @@ $start = $time;
         </div>
     </div>
     <?php include 'partials/_footer.php'; ?>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
