e9d7efc44e91b076662f4314566e6cade9d77890
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-04-28 15:13:11 +0530

    course page build started v3.006

--- course.php
+++ course.php
@@ -89,7 +89,6 @@ function minifier($code)
 <body>
     <?php
     include 'partials/_header.php';
-    include 'partials/_mailpop.php';
     ?>
 
     <div class="header">
@@ -200,7 +199,6 @@ function minifier($code)
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
         </script>
-        <script src="../assets/js/script.js"></script>
 
 </body>
 
