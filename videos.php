026e3879f6c3523c6f007f032bd6f345107f9d31
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-04-28 20:18:39 +0530

    course page build started v4.7

--- videos.php
+++ videos.php
@@ -202,20 +202,24 @@ function minifier($code)
     </div>
     <!-- sidebar start  -->
     <aside class=" sidebarBox sticky-top">
-        <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
-            aria-controls="offcanvasRight">Toggle right offcanvas</button>
-
-        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-            <div class="offcanvas-header">
-                <h5 id="offcanvasRightLabel">Offcanvas right</h5>
-                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
-                    aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body">
-                ...
+        <div class="sideHeading">
+            <p>Course Content</p>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                This is some text within a card body.
             </div>
         </div>
-
+        <div class="card">
+            <div class="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
     </aside>
     <!-- sidebar end  -->
 
