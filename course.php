9d7b3a2ffdc1271c105426de23a43128dc8e5937
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-04-28 14:45:10 +0530

    course page build started v3.002

--- course.php
+++ course.php
@@ -63,7 +63,6 @@ function minifier($code)
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="stylesheet" href="../assets/css/post.css">
     <link rel="stylesheet" href="../assets/css/vids.css">
     <link rel="stylesheet" href="../assets/css/footer.css">
     <link rel="stylesheet" href="../assets/css/bootstrap-override.css">
