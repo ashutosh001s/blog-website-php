3619c95d80e3ba2d2b1a3f833a80d2176ea2d524
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-04-28 11:15:01 +0530

    scroll progress bug fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function () {
+  myFunction()
+};
+
+function myFunction() {
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
 let newsModal = new bootstrap.Modal(document.getElementById('newsletterModal'));
 window.onload = function () {
   setTimeout(function () {
