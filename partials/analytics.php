fd1f3cf3599f77ff548977ecc36d5a3daff18045
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-04-28 20:37:40 +0530

    adsense code added

--- partials/analytics.php
+++ partials/analytics.php
@@ -7,6 +7,6 @@ echo"<!-- Global site tag (gtag.js) - Google Analytics -->
   gtag('js', new Date());
 
   gtag('config', 'UA-177387594-1');
-</script>";
-
-?>
+</script>
+<script data-ad-client="."ca-pub-4729046285466722"." async src="."https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"."></script>
+";
