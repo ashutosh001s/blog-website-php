a27fe1ac452260e521a88fcaf1cfbcfc410a693f
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-04-28 18:21:56 +0530

    course page build started v4.3

--- videos.php
+++ videos.php
@@ -173,7 +173,7 @@ function minifier($code)
       </form>';
         } else {
             echo '<div class="alert alert-success commentLog" role="alert">
-  <h4 class="alert-heading">questions</h4>
+  <h4 class="alert-heading">Discussion</h4>
   <p>Please login to start discussion.</p>
   <hr>
   <p class="mb-0">You can <a href="#" data-bs-toggle="modal" data-bs-target="#signupModal">create account</a> or <a href="#" data-bs-toggle="modal" data-bs-target="#loginModal">login</a> to post a question.</p>
