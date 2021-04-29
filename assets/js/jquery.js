769e4c87ec0c10f2db88323f778c909dedeac6a5
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-04-29 17:01:05 +0530

    course page build started v6.2

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -12,13 +12,13 @@ $('.resourses').show()
 $('.commentSection').hide()
 
 $('#btn1').click(function () {
-    $('#btn1').css("border-bottom: 2px solid black !important");
+    $('#btn1').css("border-bottom", "2px solid black !important");
     $('.resourses').show();
     $('.commentSection').hide();
 });
 
 $('#btn2').click(function () {
-    $('#btn2').css("border-bottom: 2px solid black !important");
+    $('#btn2').css("border-bottom", "2px solid black !important");
     $('.commentSection').show();
     $('.resourses').hide();
 });
