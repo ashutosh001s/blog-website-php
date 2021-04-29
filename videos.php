dc7ffb2d63ff490f7701805eba9f8a42a052ad06
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-04-29 09:07:30 +0530

    course page build started v5

--- videos.php
+++ videos.php
@@ -254,7 +254,7 @@ function minifier($code)
     $(function() {
         $("a").each(function() {
             if ($(this).attr("href") == window.location.pathname) {
-                $(this).find('.card').html().addClass("selected");
+                $(this).parent().addClass("selected");
             }
         });
     });
