6f42106c91e620f8144a3ed9bb502814dcddffe9
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-04-29 13:21:12 +0530

    course page build started v5.6

--- videos.php
+++ videos.php
@@ -203,7 +203,7 @@ function minifier($code)
     </section>
     </div>
     <!-- sidebar start  -->
-    <div class="accordion accordion-flush sidebarBox sticky-top" id="accordionFlushExample">
+    <div class="accordion accordion-flush" id="accordionFlushExample">
         <div class="accordion-item">
             <h2 class="accordion-header" id="flush-headingOne">
                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
@@ -213,7 +213,7 @@ function minifier($code)
             </h2>
             <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne"
                 data-bs-parent="#accordionFlushExample">
-                <aside class=" sticky-top">
+                <aside class=" sidebarBox sticky-top">
                     <div class="sideHeading">
                         <p>Course Content</p>
                     </div>
