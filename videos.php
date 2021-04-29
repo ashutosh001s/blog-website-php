251f4cb683e05cf372a70b358d520e638b68fff6
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-04-29 07:26:56 +0530

    videos url bug fix

--- videos.php
+++ videos.php
@@ -62,7 +62,7 @@ function minifier($code)
         integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href=".../assets/css/style.css">
+    <link rel="stylesheet" href="~/assets/css/style.css">
     <link rel="stylesheet" href=".../assets/css/post.css">
     <link rel="stylesheet" href=".../assets/css/footer.css">
     <link rel="stylesheet" href=".../assets/css/bootstrap-override.css">
@@ -71,15 +71,15 @@ function minifier($code)
     <?php include 'partials/analytics.php'; ?>
 
     <style>
-    .commentBy {
-        font-size: 18px;
-        font-family: fantasy;
-        color: #008EFF;
-    }
+        .commentBy {
+            font-size: 18px;
+            font-family: fantasy;
+            color: #008EFF;
+        }
 
-    .bg-light {
-        position: relative !important;
-    }
+        .bg-light {
+            position: relative !important;
+        }
     </style>
 
 
@@ -234,7 +234,7 @@ function minifier($code)
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
-    </script>
+        </script>
 
 </body>
 
