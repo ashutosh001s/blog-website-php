627b4900b400ce70e8e54a360a2ee7909b27103e
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-04-30 10:46:19 +0530

    course page build started v6.6

--- videos.php
+++ videos.php
@@ -1,42 +1,43 @@
 <?php
-$time = microtime();
-$time = explode(' ', $time);
-$time = $time[1] + $time[0];
-$start = $time;
-?>
-<?php include 'partials/_dbconnect.php'; ?>
-<?php
-ob_start("minifier");
-function minifier($code)
-{
-    $search = array(
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    $time = microtime();
+    $time = explode(' ', $time);
+    $time = $time[1] + $time[0];
+    $start = $time;
 
-        // Remove whitespaces after tags 
-        '/\>[^\S ]+/s',
+    include 'partials/_dbconnect.php';
 
-        // Remove whitespaces before tags 
-        '/[^\S ]+\</s',
+    ob_start("minifier");
+    function minifier($code)
+    {
+        $search = array(
 
-        // Remove multiple whitespace sequences 
-        '/(\s)+/s',
+            // Remove whitespaces after tags 
+            '/\>[^\S ]+/s',
 
-        // Removes comments
-        '/<!--(.|\s)*?-->/'
-    );
-    $replace = array('>', '<', '\\1');
-    $code = preg_replace($search, $replace, $code);
-    return $code;
-}
-?>
-<!doctype html>
+            // Remove whitespaces before tags 
+            '/[^\S ]+\</s',
+
+            // Remove multiple whitespace sequences 
+            '/(\s)+/s',
+
+            // Removes comments
+            '/<!--(.|\s)*?-->/'
+        );
+        $replace = array('>', '<', '\\1');
+        $code = preg_replace($search, $replace, $code);
+        return $code;
+    }
+
+    echo '<!doctype html>
 <html lang="en">
 
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
 
-    <!-- For seo -->
-    <?php
+    <!-- For seo -->';
+
     $id = $_GET['id'];
     $sql = "SELECT * FROM `posts` WHERE url = '$id'";
     $result = mysqli_query($conn, $sql);
@@ -54,8 +55,8 @@ function minifier($code)
     <meta name="keywords" content="' . $keywords . '">
     <meta name="author" content="' . $author . '">
     <meta name="viewport" content="width=device-width, initial-scale=1">';
-    ?>
-    <!-- Bootstrap CSS -->
+
+    echo '<!-- Bootstrap CSS -->
     <script defer src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script defer src="/assets/js/jquery.js"> </script>
 
@@ -70,10 +71,10 @@ function minifier($code)
     <link rel="stylesheet" href="/assets/css/footer.css">
     <link rel="stylesheet" href="/assets/css/bootstrap-override.css">
     <link rel="stylesheet" href="/assets/css/vids.css">
-    <link rel="stylesheet" href="/assets/css/responsive.css">
-    <?php include 'partials/analytics.php'; ?>
+    <link rel="stylesheet" href="/assets/css/responsive.css">';
+    include 'partials/analytics.php';
 
-    <style>
+    echo '<style>
     .commentBy {
         font-size: 18px;
         font-family: fantasy;
@@ -89,29 +90,29 @@ function minifier($code)
 
 </head>
 
-<body>
-    <?php
+<body>';
+
     include 'partials/_header.php';
-    ?>
-
-    <!-- contentBucket start  -->
-    <div class="contentBucket">
-        <?php
-
-        $id = $_GET['id'];
-        $play = $_GET['play'];
-        $sql = "SELECT * FROM `videos` WHERE playlist = '$play' AND url = '$id'";
-        $result = mysqli_query($conn, $sql);
-        $row = mysqli_fetch_assoc($result);
-        $title = $row['title'];
-        $content = $row['content'];
-        $videoId = $row['video_id'];
-        if ($content == "") {
-            header('Location: /404');
-        }
 
 
-        echo ' <div class="mainSection">
+    echo '<!-- contentBucket start  -->
+    <div class="contentBucket">';
+
+
+    $id = $_GET['id'];
+    $play = $_GET['play'];
+    $sql = "SELECT * FROM `videos` WHERE playlist = '$play' AND url = '$id'";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
+    $title = $row['title'];
+    $content = $row['content'];
+    $videoId = $row['video_id'];
+    if ($content == "") {
+        header('Location: /404');
+    }
+
+
+    echo ' <div class="mainSection">
             <section id="videoSection" class="videoSection">
                 <div class="videoBox">
                     <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/' . $videoId . '"
@@ -140,62 +141,61 @@ function minifier($code)
                     </div>
                 </div>
             </div>';
-        ?>
 
 
 
 
-        <?php
-        include 'partials/_dbconnect.php';
-        $url = $_SERVER['REQUEST_URI'];
-        session_start();
 
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $id = $_GET['id'];
-            $question = $_POST['question'];
-            $question = str_replace("<", "&lt;", $question);
-            $question = str_replace(">", "&gt;", $question);
-            date_default_timezone_set("Asia/Kolkata");
-            $today = date("j F, Y");
-            $username = $_SESSION['username'];
-            $useremail = $_SESSION['useremail'];
-            // $sql = "INSERT INTO `questions` (`page_no`, `question`, `question_date`) VALUES ('$id', '$question', '$today')";
-            $sql = "INSERT INTO `discussion` (`page_no`, `question_by`, `question_user_email`, `question`, `question_date`) VALUES ('$id', '$username', '$useremail', '$question', '$today')";
-            $result = mysqli_query($conn, $sql);
-        }
+    include 'partials/_dbconnect.php';
+    $url = $_SERVER['REQUEST_URI'];
+    session_start();
 
-
-
-        echo '<div class="commentSection">';
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $id = $_GET['id'];
+        $question = $_POST['question'];
+        $question = str_replace("<", "&lt;", $question);
+        $question = str_replace(">", "&gt;", $question);
+        date_default_timezone_set("Asia/Kolkata");
+        $today = date("j F, Y");
+        $username = $_SESSION['username'];
+        $useremail = $_SESSION['useremail'];
+        // $sql = "INSERT INTO `questions` (`page_no`, `question`, `question_date`) VALUES ('$id', '$question', '$today')";
+        $sql = "INSERT INTO `discussion` (`page_no`, `question_by`, `question_user_email`, `question`, `question_date`) VALUES ('$id', '$username', '$useremail', '$question', '$today')";
+        $result = mysqli_query($conn, $sql);
+    }
 
 
 
-        if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-            echo '<form action="' . $url . '" method = "POST">
+    echo '<div class="commentSection">';
+
+
+
+    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+        echo '<form action="' . $url . '" method = "POST">
         <div class="form-floating">
           <label for="commentSection">Discussion</label>
           <textarea class="form-control" placeholder="Leave a question here" name= "question" id="questionSection" style="height: 100px" required></textarea>
         </div>
         <button type="submit" class="btn btn-light">Submit</button>
       </form>';
-        } else {
-            echo '<div class="alert alert-success commentLog" role="alert">
+    } else {
+        echo '<div class="alert alert-success commentLog" role="alert">
   <h4 class="alert-heading">Discussion</h4>
   <p>Please login to start discussion.</p>
   <p class="mb-0">You can <a href="#" data-bs-toggle="modal" data-bs-target="#signupModal">create account</a> or <a href="#" data-bs-toggle="modal" data-bs-target="#loginModal">login</a> to post a question.</p>
 </div>';
-        }
+    }
 
 
-        $sql = "SELECT * FROM `discussion` WHERE page_no = '$id' ORDER BY `question_no` DESC";
-        $result = mysqli_query($conn, $sql);
-        while ($row = mysqli_fetch_assoc($result)) {
-            $question = $row['question'];
-            $questionBy = $row['question_by'];
-            $questionDate = $row['question_date'];
+    $sql = "SELECT * FROM `discussion` WHERE page_no = '$id' ORDER BY `question_no` DESC";
+    $result = mysqli_query($conn, $sql);
+    while ($row = mysqli_fetch_assoc($result)) {
+        $question = $row['question'];
+        $questionBy = $row['question_by'];
+        $questionDate = $row['question_date'];
 
 
-            echo '<div class="media comments">
+        echo '<div class="media comments">
         
             <div class="media-body">
               <div style="display: flex; align-items: center;">
@@ -207,31 +207,31 @@ function minifier($code)
             </div>
           
           </div>';
-        }
-        ?>
-    </div>
+    }
+
+    echo '</div>
     <!-- sidebar start  -->
     <aside id="contentBar" class=" contentCarryBox sticky-top">
         <div class="sideHeading">
             <p><img src="/assets/img/icons/hide_image_black_24dp.svg" id="hidePlayer" style="margin-right: 18px;">Course
                 Content</p>
-        </div>
-
-        <?php
-        $no = 0;
-        $sql = "SELECT * FROM `videos` WHERE playlist = '$play'";
-        $result = mysqli_query($conn, $sql);
-        while ($row = mysqli_fetch_assoc($result)) {
-
-            $no++;
-            $title = $row['title'];
-            $url = $row['url'];
-            $playlsit = $row['playlist'];
-            $host = $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-            $domain = $_SERVER['SERVER_NAME'];
+        </div>';
 
 
-            echo '<a class = "vAnchor" href="/videos/' . $playlsit . '/' . $url . '" style=" text-decoration: none; "> 
+    $no = 0;
+    $sql = "SELECT * FROM `videos` WHERE playlist = '$play'";
+    $result = mysqli_query($conn, $sql);
+    while ($row = mysqli_fetch_assoc($result)) {
+
+        $no++;
+        $title = $row['title'];
+        $url = $row['url'];
+        $playlsit = $row['playlist'];
+        $host = $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+        $domain = $_SERVER['SERVER_NAME'];
+
+
+        echo '<a class = "vAnchor" href="/videos/' . $playlsit . '/' . $url . '" style=" text-decoration: none; "> 
                     <div class="card">
                         <div class="card-body">
                         <h5 class="card-title"><span style="margin-right: 7px;">' . $no . '.</span>' . $title . '</h5> 
@@ -239,20 +239,20 @@ function minifier($code)
                         </div>
                     </div>
                     </a>';
-        }
-        ?>
+    }
 
-    </aside>
+
+    echo ' </aside>
     <!-- sidebar end  -->
     </section>
     </div>
 
 
     </div>
-    <!-- contentBucket end  -->
+    <!-- contentBucket end  -->';
 
-    <?php include 'partials/_footer.php'; ?>
-    <!-- Option 1: Bootstrap Bundle with Popper -->
+    include 'partials/_footer.php';
+    echo ' <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
     </script>
@@ -261,8 +261,10 @@ function minifier($code)
 
 </body>
 
-</html>
-<?php
-ob_end_flush();
+</html>';
+
+    ob_end_flush();
+
+    include 'partials/tracker.php';
+}
 ?>
-<?php include 'partials/tracker.php'; ?>
