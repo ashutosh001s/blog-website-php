74bae281537f8c7fe4bb769e3b9d87786284c2b0
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-05-01 19:32:59 +0530

    making add video page v8

--- dashboard/components-video-post.php
+++ dashboard/components-video-post.php
@@ -34,7 +34,7 @@
             <!-- End Page Header -->
             <div class="row">
               <?php
-               $sql = "SELECT * FROM `posts`";
+               $sql = "SELECT * FROM `videos`";
                $result = mysqli_query($conn, $sql);
                while ($row = mysqli_fetch_assoc($result)) {
                  $content = $row['content'];
@@ -50,26 +50,31 @@
          
                  $srno = $row['sr_no'];
                  $image = $row['image'];
-                 $cate = $row['category'];
+                 $cate = $row['playlist'];
                  $title = $row['title'];
                  $url = $row['url'];
                 
-                 echo '<div class="col-lg-3 col-md-6 col-sm-12 mb-4">
-                 <div class="card card-small card-post h-100">
-                   <img class="card-post__image" src="/assets/img/cover/' . $image . '" style="height: 250px;"></img>
-                   <div class="card-body">
-                     <h5 class="card-title">
-                       <a class="text-fiord-blue" href="update-post.php?post='.$url.'">' . $title . '</a>
-                     </h5>
-                     <p class="card-text">' . substr($content, 0, 70) . '</p>
+                 echo '<div class="col-lg-4">
+                   <div class="card card-small card-post mb-4">
+                     <div class="card-body">
+                       <h5 class="card-title">Had denoting properly jointure which well books beyond</h5>
+                       <p class="card-text text-muted"> In said to of poor full be post face snug. Introduced imprudence see say unpleasing devonshire acceptance son. Exeter longer...</p>
+                     </div>
+                     <div class="card-footer border-top d-flex">
+                       <div class="card-post__author d-flex">
+                         <a href="#" class="card-post__author-avatar card-post__author-avatar--small" style="background-image: url();">Written by James Khan</a>
+                         <div class="d-flex flex-column justify-content-center ml-3">
+                           <span class="card-post__author-name">James Khan</span>
+                           <small class="text-muted">21 March 2011</small>
+                         </div>
+                       </div>
+                       <div class="my-auto ml-auto">
+                         <a class="btn btn-sm btn-white" href="#">
+                           <i class="far fa-bookmark mr-1"></i> Bookmark </a>
+                       </div>
+                     </div>
                    </div>
-                   <div class="card-footer text-muted border-top py-3">
-                     <span class="d-inline-block">Category
-                       <a class="text-fiord-blue" href="#">'.$cate.'</a>
-                     </span>
-                   </div>
-                 </div>
-               </div>';
+                 </div>';
                 
                 }
               ?>
