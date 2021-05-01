551e74970b44e6e402f07e6508d03dadcff60ba4
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-05-01 10:36:50 +0530

    adding new dashboard making table page

--- dashboard/tables.php
+++ dashboard/tables.php
@@ -231,113 +231,46 @@
                     <table class="table mb-0">
                       <thead class="bg-light">
                         <tr>
-                          <th scope="col" class="border-0">#</th>
-                          <th scope="col" class="border-0">First Name</th>
-                          <th scope="col" class="border-0">Last Name</th>
-                          <th scope="col" class="border-0">Country</th>
-                          <th scope="col" class="border-0">City</th>
-                          <th scope="col" class="border-0">Phone</th>
+                          <th scope="col" class="border-0">Sr No</th>
+                          <th scope="col" class="border-0">Ip</th>
+                          <th scope="col" class="border-0">Email</th>
+                          <th scope="col" class="border-0">Url</th>
+                          <th scope="col" class="border-0">Load Time</th>
+                          <th scope="col" class="border-0">Visit Time</th>
                         </tr>
                       </thead>
-                      <tbody>
+                      <?php
+                      include 'partials/_dbconnect.php';
+                      $sr_no = 0;
+                      $sql = "SELECT * FROM `visitors` ORDER BY `v_no` DESC";
+                      $result = mysqli_query($conn, $sql);
+                      while ($row = mysqli_fetch_assoc($result)) {
+                        $sr_no++;
+                        $v_ip = $row['v_ip'];
+                        $v_email = $row['v_email'];
+                        $v_url = $row['v_url'];
+                        $v_load_time = $row['load_time'];
+                        $v_time = $row['v_time'];
+                      echo'<tbody>
                         <tr>
-                          <td>1</td>
-                          <td>Ali</td>
-                          <td>Kerry</td>
-                          <td>Russian Federation</td>
-                          <td>Gdańsk</td>
-                          <td>107-0339</td>
+                          <td>' . $sr_no . '</td>
+                          <td>' . $v_ip . '</td>
+                          <td>' . $v_email . '</td>
+                          <td>' . $v_url . '</td>
+                          <td>' . $v_load_time . '</td>
+                          <td>' . $v_time . '</td>
                         </tr>
-                        <tr>
-                          <td>2</td>
-                          <td>Clark</td>
-                          <td>Angela</td>
-                          <td>Estonia</td>
-                          <td>Borghetto di Vara</td>
-                          <td>1-660-850-1647</td>
-                        </tr>
-                        <tr>
-                          <td>3</td>
-                          <td>Jerry</td>
-                          <td>Nathan</td>
-                          <td>Cyprus</td>
-                          <td>Braunau am Inn</td>
-                          <td>214-4225</td>
-                        </tr>
-                        <tr>
-                          <td>4</td>
-                          <td>Colt</td>
-                          <td>Angela</td>
-                          <td>Liberia</td>
-                          <td>Bad Hersfeld</td>
-                          <td>1-848-473-7416</td>
-                        </tr>
-                      </tbody>
+                        
+                      </tbody>';
+                      }
+                      ?>
                     </table>
                   </div>
                 </div>
               </div>
             </div>
             <!-- End Default Light Table -->
-            <!-- Default Dark Table -->
-            <div class="row">
-              <div class="col">
-                <div class="card card-small overflow-hidden mb-4">
-                  <div class="card-header bg-dark">
-                    <h6 class="m-0 text-white">Inactive Users</h6>
-                  </div>
-                  <div class="card-body p-0 pb-3 bg-dark text-center">
-                    <table class="table table-dark mb-0">
-                      <thead class="thead-dark">
-                        <tr>
-                          <th scope="col" class="border-bottom-0">#</th>
-                          <th scope="col" class="border-bottom-0">First Name</th>
-                          <th scope="col" class="border-bottom-0">Last Name</th>
-                          <th scope="col" class="border-bottom-0">Country</th>
-                          <th scope="col" class="border-bottom-0">City</th>
-                          <th scope="col" class="border-bottom-0">Phone</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td>1</td>
-                          <td>Graham</td>
-                          <td>Brent</td>
-                          <td>Benin</td>
-                          <td>Ripabottoni</td>
-                          <td>1-512-760-9094</td>
-                        </tr>
-                        <tr>
-                          <td>2</td>
-                          <td>Clark</td>
-                          <td>Angela</td>
-                          <td>Estonia</td>
-                          <td>Borghetto di Vara</td>
-                          <td>1-660-850-1647</td>
-                        </tr>
-                        <tr>
-                          <td>3</td>
-                          <td>Wylie</td>
-                          <td>Joseph</td>
-                          <td>Korea, North</td>
-                          <td>Guelph</td>
-                          <td>325-4351</td>
-                        </tr>
-                        <tr>
-                          <td>4</td>
-                          <td>Garth</td>
-                          <td>Clementine</td>
-                          <td>Indonesia</td>
-                          <td>Narcao</td>
-                          <td>722-8264</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <!-- End Default Dark Table -->
+           
           </div>
           <footer class="main-footer d-flex p-2 px-3 bg-white border-top">
             <ul class="nav">
