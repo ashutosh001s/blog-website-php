dc8f4eb34e14cd2d081db8aabd13d7dcf761099a
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-05-01 19:39:07 +0530

    making add video page v9

--- dashboard/components-video-post.php
+++ dashboard/components-video-post.php
@@ -50,27 +50,25 @@
          
                  $srno = $row['sr_no'];
                  $image = $row['image'];
-                 $cate = $row['playlist'];
+                 $playlist = $row['playlist'];
                  $title = $row['title'];
                  $url = $row['url'];
                 
                  echo '<div class="col-lg-4">
                    <div class="card card-small card-post mb-4">
                      <div class="card-body">
-                       <h5 class="card-title">Had denoting properly jointure which well books beyond</h5>
-                       <p class="card-text text-muted"> In said to of poor full be post face snug. Introduced imprudence see say unpleasing devonshire acceptance son. Exeter longer...</p>
+                       <h5 class="card-title">'.$title.'</h5>
+                       <p class="card-text text-muted">'.$content.'</p>
                      </div>
                      <div class="card-footer border-top d-flex">
                        <div class="card-post__author d-flex">
-                         <a href="#" class="card-post__author-avatar card-post__author-avatar--small" style="background-image: url();">Written by James Khan</a>
                          <div class="d-flex flex-column justify-content-center ml-3">
-                           <span class="card-post__author-name">James Khan</span>
-                           <small class="text-muted">21 March 2011</small>
+                           <span class="card-post__author-name">'.$playlist.'</span>
                          </div>
                        </div>
                        <div class="my-auto ml-auto">
                          <a class="btn btn-sm btn-white" href="#">
-                           <i class="far fa-bookmark mr-1"></i> Bookmark </a>
+                           <i class="far mode_edit_outline mr-1"></i> Edit </a>
                        </div>
                      </div>
                    </div>
