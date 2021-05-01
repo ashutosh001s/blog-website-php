0249705cc0b84a3b7f056c9100992b269e80d0e2
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-05-01 12:43:33 +0530

    checking security on index page of dashboard

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,24 +1,4 @@
-<?php
-session_start();
-include '../partials/_dbconnect.php';
-if (isset($_SESSION['loggedin'])  && $_SESSION['loggedin'] == true) {
-  $useremail = $_SESSION['useremail'];
-  $sql = "SELECT * FROM `users` WHERE user_roles = 'administrator'";
-  $result = mysqli_query($conn, $sql);
-  $row = mysqli_fetch_assoc($result);
-  $adminEmail = $row['user_email'];
-
-  if ($useremail == $adminEmail) {
-  
-
-  }else{
-    header('Location: /404');
-  } 
-}
-else{
-  header('Location: /404');
-}
-?>
+<?php require './impfiles/adminAuth.php'; ?>
 <!doctype html>
 <html class="no-js h-100" lang="en">
   <head>
