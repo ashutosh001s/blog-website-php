2cd1c68d00b9d45fcac18b5c0c24932bba5f1c1b
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-05-01 20:07:26 +0530

    making update video

--- partials/_update-video.php
+++ partials/_update-video.php
@@ -2,13 +2,12 @@
 include '_dbconnect.php';
 $url = $_POST['url'];
 $title = $_POST['title'];
-$category = $_POST['category'];
+$playlist = $_POST['playlist'];
 $tags = $_POST['tags'];
-$readTime = $_POST['read'];
+$videoId = $_POST['video_id'];
 $description = $_POST['discription'];
 $keywords = $_POST['keywords'];
-$author = $_POST['author'];
-$image = $_POST['image'];
+$creator = $_POST['creator'];
 date_default_timezone_set("Asia/Kolkata");
 $date = date("j F, Y");
 $content = $_POST['content'];
@@ -17,7 +16,7 @@ $content = str_replace("'", "	&apos;" , $content);
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
-    $sql = "UPDATE `posts` SET `title` = '$title', `content` = '$content', `category` = '$category', `tags` = '$tags', `date` = '$date', `description` = '$description', `keywords` = '$keywords', `author` = '$author' , `read_time` = '$readTime' WHERE `posts`.`url` = '$url'";
+    $sql = "UPDATE `posts` SET `title` = '$title', `content` = '$content', `playlist` = '$playlist', `tags` = '$tags', `date` = '$date', `description` = '$description', `keywords` = '$keywords', `creator` = '$creator' , `video_id` = '$videoId' WHERE `posts`.`url` = '$url'";
     $result = mysqli_query($conn, $sql);
 
     if($result){
