cf7b72e4e349376baed16a9e1da86601f237f4b2
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-05-02 18:38:49 +0530

    making verification system on signup

---
+++ partials/_verifyEmail.php
@@ -0,0 +1,25 @@
+<?php
+
+   include '_dbconnect.php';
+   $email = $_POST['email'];
+   $pass = $_POST['otp'];
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    $sql = "SELECT * FROM `users` WHERE user_email = '$email' and `otp` = '$otp'";
+    $result = mysqli_query($conn , $sql);
+    $numRows = mysqli_num_rows($result);
+    
+    if($numRows == 1){
+        $sql = "UPDATE `users` SET `verification` = '1' WHERE `users`.`user_email` = '$email'";
+        $result = mysqli_query($conn, $sql);
+
+        if($result){
+
+            $sql = "UPDATE `users` SET `otp` = '' WHERE `users`.`user_email` = '$email'";
+            $result = mysqli_query($conn , $sql);
+            
+        }
+    }
+}
+?>
