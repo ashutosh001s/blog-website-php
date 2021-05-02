28dc370e75ce4f2a1ebe7fffe3cd1f6623e763ad
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-05-02 16:39:15 +0530

    making forgot password process smooth

--- account/reset.php
+++ account/reset.php
@@ -38,7 +38,11 @@
 							
 							$diff = abs(strtotime($date2) - strtotime($date1));
 
-							echo $diff;
+							$years = floor($diff / (365*60*60*24));
+$months = floor(($diff - $years * 365*60*60*24) / (30*60*60*24));
+$days = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24));
+
+printf("%d years, %d months, %d days\n", $years, $months, $days);
 
 				
 
