f28c4730198d72211f0b5c527f45a6756eebef60
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-05-02 14:17:06 +0530

    making forgot pass DEBUG

--- partials/_resetPass.php
+++ partials/_resetPass.php
@@ -13,9 +13,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $numRows = mysqli_num_rows($result1);
     echo $numRows;
     echo $email;
-    // if($numRows == 1){
+    if($numRows == 1){
         
-
+        echo "yes";
     //     $hash = password_hash($newPass, PASSWORD_DEFAULT);
     //     $sql = "UPDATE `users` SET `user_pass` = '$hash' WHERE `users`.`user_email` = '$email'";
     //     $result2 = mysqli_query($conn, $sql);
@@ -25,12 +25,12 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     //             echo 'password reset unsuccessful';
     //         }
 
-    // }else{
-    //     echo 'no email found';
-    //     // echo $email;
+    }else{
+        echo 'no email found';
+        // echo $email;
         
-    //     // echo $token;
-    // }
+        // echo $token;
+    }
 }
 
 ?>
