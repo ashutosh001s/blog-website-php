7e64eb7de163ea24b5e751f59fbe5e0839b2e98f
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-05-02 14:40:38 +0530

    making forgot pass DEBUG finally

--- account/reset.php
+++ account/reset.php
@@ -24,7 +24,7 @@
 							$email = $_GET['email'];
 							$token = $_GET['token'];
 
-							include '_dbconnect.php';
+							include '/partials/_dbconnect.php';
 							$sql = "SELECT * FROM `users` WHERE user_email = '$email' AND forgot_token = '$token'";
    							$result = mysqli_query($conn , $sql);
 							$numRows = mysqli_num_rows($result);
