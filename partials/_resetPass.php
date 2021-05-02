219bd62c4e68ac8dada770e4557395bb653c0c8f
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-05-02 14:14:19 +0530

    making forgot pass DEBUG

--- partials/_resetPass.php
+++ partials/_resetPass.php
@@ -8,29 +8,29 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $email = $_POST['email'];
     $token = $_POST['token'];
 
-    $sql = "SELECT * FROM `users` WHERE `user_email` = '$email' AND `forgot_token` = '$token'";
+    $sql = "SELECT * FROM `users` WHERE `user_email` = '$email'";
     $result1 = mysqli_query($conn , $sql);
     $numRows = mysqli_num_rows($result1);
     echo $numRows;
     
-    if($numRows == 1){
+    // if($numRows == 1){
         
 
-        $hash = password_hash($newPass, PASSWORD_DEFAULT);
-        $sql = "UPDATE `users` SET `user_pass` = '$hash' WHERE `users`.`user_email` = '$email'";
-        $result2 = mysqli_query($conn, $sql);
-            if($result2){
-                echo 'password reset successful';
-            }else{
-                echo 'password reset unsuccessful';
-            }
+    //     $hash = password_hash($newPass, PASSWORD_DEFAULT);
+    //     $sql = "UPDATE `users` SET `user_pass` = '$hash' WHERE `users`.`user_email` = '$email'";
+    //     $result2 = mysqli_query($conn, $sql);
+    //         if($result2){
+    //             echo 'password reset successful';
+    //         }else{
+    //             echo 'password reset unsuccessful';
+    //         }
 
-    }else{
-        echo 'no email found';
-        // echo $email;
+    // }else{
+    //     echo 'no email found';
+    //     // echo $email;
         
-        // echo $token;
-    }
+    //     // echo $token;
+    // }
 }
 
 ?>
