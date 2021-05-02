548a9c3b021bdd5c81eaa9a1d6a6964a18dac395
Ashutosh singh <ashutoshcharan123@gmail.com>
2021-05-02 14:20:56 +0530

    making forgot pass DEBUG

--- partials/_resetPass.php
+++ partials/_resetPass.php
@@ -3,33 +3,29 @@
 
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-
+    include '_dbconnect.php';
+    
     $newPass = $_POST['newPassword'];
     $email = $_POST['email'];
     $token = $_POST['token'];
 
-    $sql = "SELECT * FROM `users` WHERE `user_email` = 'ashutoshcharan121@gmail.com'";
-    $result1 = mysqli_query($conn , $sql);
-    $numRows = mysqli_num_rows($result1);
-    echo $numRows;
-    echo $email;
+    $sql = "SELECT * FROM `users` WHERE user_email = '$email' AND forgot_token = '$token'";
+    $result = mysqli_query($conn , $sql);
+    $numRows = mysqli_num_rows($result);
+    
     if($numRows == 1){
-        
-        echo "yes";
-    //     $hash = password_hash($newPass, PASSWORD_DEFAULT);
-    //     $sql = "UPDATE `users` SET `user_pass` = '$hash' WHERE `users`.`user_email` = '$email'";
-    //     $result2 = mysqli_query($conn, $sql);
-    //         if($result2){
-    //             echo 'password reset successful';
-    //         }else{
-    //             echo 'password reset unsuccessful';
-    //         }
+
+        $hash = password_hash($newPass, PASSWORD_DEFAULT);
+        $sql = "UPDATE `users` SET `user_pass` = '$hash' WHERE `users`.`user_email` = '$email'";
+        $result = mysqli_query($conn, $sql);
+            if($result){
+                echo 'password reset successful';
+            }else{
+                echo 'password reset unsuccessful';
+            }
 
     }else{
         echo 'no email found';
-        // echo $email;
-        
-        // echo $token;
     }
 }
 
